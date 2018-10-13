3c39b7042d7f519cee520faf97e0f96199ab868a
Marcin Bukryj <buryj666@gmail.com>
2018-10-13 11:21:42 +0200

    fake news classifier

---
+++ DataCamp_Natural_Language_Processing_Fundamentals/3.3.1.French_NER_with_polyglot_I.py
@@ -0,0 +1,34 @@
+'''In this exercise and the next, you'll use the polyglot library to identify French entities. 
+The library functions slightly differently than spacy, so you'll use a few of the new things you learned in the last video to display the named entity text and category.
+
+You have access to the full article string in article. Additionally, the Text class of polyglot has been imported from polyglot.text.'''
+
+#TASK
+# Create a new Text object called txt.
+# Iterate over txt.entities and print each entity, ent.
+# Print the type() of ent.
+
+# Create a new text object using Polyglot's Text class: txt
+txt = 
+
+# Print each of the entities found
+for ____ in ____:
+    ____
+    
+# Print the type of ent
+____
+
+
+
+
+
+#SOLUTION
+# Create a new text object using Polyglot's Text class: txt
+txt = Text(article)
+
+# Print each of the entities found
+for ent in txt.entities:
+    print(ent)
+    
+# Print the type of ent
+print(type(ent))
